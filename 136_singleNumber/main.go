cd7244b0939add9b489e629fc6bfc227481ca325
alexChernyatiev <alex.chernyatiev@gmail.com>
2023-11-10 13:06:14 +0300

    Added singleNumber

---
+++ 136_singleNumber/main.go
@@ -0,0 +1,35 @@
+package main
+
+//Solution with XOR
+//'x^y=y^x', 'x^0=x' and 'x^x=0' so we can remove pairs of numbers and receive result
+func singleNumber(nums []int) int {
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum = sum ^ nums[i]
+	}
+
+	return sum
+}
+
+//Solution with using Map time: O(n), memory: O(n)
+/*func singleNumber(nums []int) int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		_, ok := m[nums[i]]
+		if ok {
+			delete(m, nums[i])
+		} else {
+			m[nums[i]] = 1
+		}
+	}
+
+	v := make([]int, 0, len(m))
+	for key := range m {
+		v = append(v, key)
+	}
+	return v[0]
+}*/
+
+func main() {
+	print("\n", singleNumber([]int{1, 2, 2, 1, 4, 3, 4, 5, 5}))
+}
